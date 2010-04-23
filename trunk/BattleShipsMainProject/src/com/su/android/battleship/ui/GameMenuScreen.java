e5014d332bb61bf28f6712924d9f1b74f52611ec
nikolakosev <nikolakosev@45fd3aa4-62c5-c62a-3edd-0140abc20f17>
2010-04-23 07:21:01 +0000

    game menu screen

---
+++ trunk/BattleShipsMainProject/src/com/su/android/battleship/ui/GameMenuScreen.java
@@ -0,0 +1,49 @@
+package com.su.android.battleship.ui;
+
+import com.su.android.battleship.R;
+import com.su.android.battleship.ui.tutorials.TutorialsScreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class GameMenuScreen extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_menu);
+		
+		Button buttonSingle = (Button) findViewById(R.id.ButtonSingle);
+		buttonSingle.setOnClickListener(this);
+		
+		Button buttonMulti = (Button) findViewById(R.id.ButtonMulti);
+		buttonMulti.setOnClickListener(this);
+		
+		Button buttonTutorials = (Button) findViewById(R.id.ButtonTutorials);
+		buttonTutorials.setOnClickListener(this);
+		
+		Button buttonBack = (Button) findViewById(R.id.ButtonBack);
+		buttonBack.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.ButtonSingle:
+			break;
+		case R.id.ButtonMulti:
+			break;
+		case R.id.ButtonTutorials:
+			Intent intent = new Intent(this, TutorialsScreen.class);
+			startActivity(intent);
+			break;
+		case R.id.ButtonBack:
+			break;
+		}
+	}
+
+}
--- trunk/BattleShipsMainProject/src/com/su/android/battleship/ui/MainMenuScreen.java
+++ trunk/BattleShipsMainProject/src/com/su/android/battleship/ui/MainMenuScreen.java
@@ -1,30 +1,48 @@
 package com.su.android.battleship.ui;
 
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.su.android.battleship.R;
-import com.su.android.battleship.ui.tutorials.TutorialsScreen;
 
-public class MainMenuScreen extends Activity {
+public class MainMenuScreen extends Activity implements OnClickListener {
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_menu);
 		
-		Button continueButton = (Button) findViewById(R.id.ButtonPlay);
-		continueButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				 Intent mIntent = new Intent(view.getContext(),TutorialsScreen.class);
-				 //intent is configured so that the next Activity will display load_screen in the beginning
-//				 mIntent.putExtra("loading_screen", true);
-				 startActivity(mIntent);				
-			}
-		});
+		Button buttonPlay = (Button) findViewById(R.id.ButtonPlay);
+		buttonPlay.setOnClickListener(this);
+		
+		Button buttonOptions = (Button) findViewById(R.id.ButtonOptions);
+		buttonOptions.setOnClickListener(this);
+		
+		Button buttonAbout = (Button) findViewById(R.id.ButtonAbout);
+		buttonAbout.setOnClickListener(this);
+		
+		Button buttonQuit = (Button) findViewById(R.id.ButtonQuit);
+		buttonQuit.setOnClickListener(this);
 	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.ButtonPlay:
+			Intent intent = new Intent(this, GameMenuScreen.class);
+			startActivity(intent);
+			break;
+		case R.id.ButtonOptions:
+			break;
+		case R.id.ButtonAbout:
+			break;
+		case R.id.ButtonQuit:
+			break;
+		}
+	}
+	
 }
