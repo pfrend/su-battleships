c09dbc552b7fc4d7b40db62e8a77eb3c76dbe23f
vslkonstantinov <vslkonstantinov@45fd3aa4-62c5-c62a-3edd-0140abc20f17>
2010-04-24 17:52:53 +0000

    adopted logic for loading screen - MainMenuScreen Activity is changed significantly , the old one is preserved in a MainMenuScreen_Backup.java_ file

--- trunk/BattleShipsMainProject/src/com/su/android/battleship/ui/MainMenuScreen.java
+++ trunk/BattleShipsMainProject/src/com/su/android/battleship/ui/MainMenuScreen.java
@@ -2,10 +2,14 @@ package com.su.android.battleship.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.su.android.battleship.R;
 
@@ -13,10 +17,17 @@ import com.su.android.battleship.R;
  * First application screen. The main menu - Play, Options, Credits, Quit
  * 
  * @author Nikola
- *
+ * 
  */
 public class MainMenuScreen extends Activity implements OnClickListener {
-	
+
+	/**
+	 * This is configuration flag - whether to show loading screen or not
+	 * TODO : design and implement loading screen as a self containing widget that can be attached 
+	 * to every activity configured to support loading screen 
+	 */
+	private static final boolean isLoadingScreen = true;
+
 	/**
 	 * Called when activity is starting
 	 * 
@@ -25,25 +36,42 @@ public class MainMenuScreen extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// start gameLoading thread
+		GameLoadingThread gameLoadingThread = new GameLoadingThread(handler);
+		gameLoadingThread.execute("Loading...");
+
+		if (isLoadingScreen) {
+			displayLoadingScreen();
+		} else {
+			displayGameMenu();
+		}
+	}
+
+	/**
+	 * This method holds the logic for loading the main game menu
+	 */
+	private void displayGameMenu() {
 		setContentView(R.layout.main_menu);
-		
+
 		Button buttonPlay = (Button) findViewById(R.id.ButtonPlay);
 		buttonPlay.setOnClickListener(this);
-		
+
 		Button buttonOptions = (Button) findViewById(R.id.ButtonOptions);
 		buttonOptions.setOnClickListener(this);
-		
+
 		Button buttonAbout = (Button) findViewById(R.id.ButtonAbout);
 		buttonAbout.setOnClickListener(this);
-		
+
 		Button buttonQuit = (Button) findViewById(R.id.ButtonQuit);
 		buttonQuit.setOnClickListener(this);
-	}
+	}	
 
 	/**
 	 * Called when a menu button has been clicked
 	 * 
-	 * @param v button instance
+	 * @param v
+	 *            button instance
 	 */
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -61,4 +89,149 @@ public class MainMenuScreen extends Activity implements OnClickListener {
 		}
 	}
 	
+	//==============================================================================================================
+	//Logic supporting loading screen
+
+	private ProgressBar progressBar;
+	private LoadScreenAsyncTask progressBarTask;
+
+
+	// Define the Handler that receives messages from the gameLoading thread and
+	// update the
+	// speed of the progressBar thread
+	final Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			if (!isLoadingScreen) {
+				return;
+			}
+			boolean isLoaded = msg.getData().getBoolean("isLoaded");
+			if (isLoaded) {
+				progressBarTask.setStatus(LoadScreenAsyncTask.STATUS_FINISHED);
+			}
+		}
+	};	
+	
+	/**
+	 * This method holds the logic for loading the loading_screen Remember that
+	 * this Activity is configurable and the call to this method can be disabled
+	 */
+	private void displayLoadingScreen() {
+		setContentView(R.layout.progressbar_loadscreen);
+
+		// start progressBar worker thread 
+		progressBarTask = new LoadScreenAsyncTask();
+		progressBarTask.execute("Loading");
+	}
+
+	
+	/**
+	 * This class represents a background worker thread that will do all the
+	 * game loading job.This is the common and desirable behavior - the UI
+	 * thread should not be charged with back-end calculations.They should be
+	 * transfered to another thread instead. This thread communicates with the
+	 * progressBarUpdate thread via a Handler objects
+	 * 
+	 * @author vasko
+	 * 
+	 */
+	private class GameLoadingThread extends AsyncTask<Object, Void, Void> {
+
+		private Handler mHandler;
+
+		public GameLoadingThread(Handler h) {
+			mHandler = h;
+		}
+
+		@Override
+		protected Void doInBackground(Object... params) {
+			// simulate activity here
+
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+			}
+
+			Message msg = mHandler.obtainMessage();
+			Bundle b = new Bundle();
+			b.putBoolean("isLoaded", true);
+			msg.setData(b);
+			mHandler.sendMessage(msg);
+			return null;
+		}
+
+	}
+
+	/**
+	 * This class represents background thread responsible for ProgressBar updates.
+	 * 
+	 * @author vasko
+	 */
+	private class LoadScreenAsyncTask extends
+			AsyncTask<String, Integer, String> {
+
+		private static final int MAX_PROGRESS_STEPS = 15;
+		private int status = STATUS_RUNNING_NORMAL;
+		public static final int STATUS_RUNNING_NORMAL = 1;;
+		public static final int STATUS_RUNNING_FAST = 2;
+		public static final int STATUS_FINISHED = 3;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			setContentView(R.layout.progressbar_loadscreen);
+			progressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
+			progressBar.setMax(MAX_PROGRESS_STEPS);
+			progressBar.setProgressDrawable(getResources().getDrawable(
+					R.drawable.progress_horizontal));
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+			progressBar.setProgress(progress[0]);
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			int i = 0;
+			while (status == STATUS_RUNNING_NORMAL) {
+				try {
+					Thread.sleep(200);
+					i++;
+				} catch (InterruptedException e) {
+
+				}
+				publishProgress(i);
+			}
+			while (i < MAX_PROGRESS_STEPS) {
+				try {
+					Thread.sleep(20);
+					i++;
+				} catch (InterruptedException e) {
+
+				}
+				publishProgress(i);
+			}
+			return "MyAsyncTask done successfully";
+		}
+
+		/**
+		 * Executed by UI thread after task has finished its job
+		 */
+		protected void onPostExecute(String result) {
+			displayGameMenu();
+		}
+
+		/**
+		 * This method is used to tell the thread whether it is time to force
+		 * faster progress of the progressBar and whether it is time to
+		 * terminate
+		 * 
+		 * @param status
+		 */
+		public void setStatus(int status) {
+			this.status = status;
+		}
+
+	}
+
 }
