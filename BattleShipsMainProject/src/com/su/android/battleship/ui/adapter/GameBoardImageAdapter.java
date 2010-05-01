89771dd6f77127fd26ed3a1d78130ffeb6cde1b8
vslkonstantinov <vslkonstantinov@45fd3aa4-62c5-c62a-3edd-0140abc20f17>
2010-05-01 00:28:26 +0000

    fixing the "upper left corner" bug - work around notes can be found in the code documentation in "GameBoardImageAdapter.java"

--- BattleShipsMainProject/src/com/su/android/battleship/ui/adapter/GameBoardImageAdapter.java
+++ BattleShipsMainProject/src/com/su/android/battleship/ui/adapter/GameBoardImageAdapter.java
@@ -3,6 +3,7 @@ package com.su.android.battleship.ui.adapter;
 import com.su.android.battleship.R;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -24,7 +25,12 @@ public class GameBoardImageAdapter extends BaseAdapter {
      * Very important method - this tells android UI framework how many times to call getView
      */
     public int getCount() {
+    	Log.d("ImageViewAdapter","geCount(): "+FIELDS_COUNT);
         return FIELDS_COUNT;
+    }
+    
+    public int getViewTypeCount(){
+    	return FIELDS_COUNT;
     }
 
     public Object getItem(int position) {
@@ -45,9 +51,29 @@ public class GameBoardImageAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-        imageView.setImageResource(boardPictures[0]);
         
-        boardFields[position] = imageView;
+        imageView.setImageResource(boardPictures[0]);
+        Log.d("ImageViewAdapter","Position is: "+position + " and convertView: "+convertView+" and parent: "+parent);
+        
+        
+        /**
+         * Due to the splendid GridView implementation of the Google team , there are no guarantees how many times
+         * this method will be called for every view.In order to achieve some sort of optimizations , this method
+         * is called multiple times for the first gridView element (but only in this particular case). For some reason, the 
+         * second call WILL , but the third MAY NOT use the convertView and thus , a new ImageView will be created.So the first
+         * itemView reference will be lost.The displayed ItemView for every position in the GridView however is the returned itemView
+         * form the FIRST call to this method.And if we store the ItemView references each time this method is called, the old
+         * ones will be lost if this method is called THREE times for the same position, which in the end causes the bug.
+         * So in conclusion either the Google team had something messed up with the implementation , or , more likely , the 
+         * ImageAdapter is not the best way to handle this kind of situation.One guess is that one can never know for sure , before
+         * one learns and understands Google's Activity/View Design Model in deep details - which can obviously be assumed to be
+         * impossible task if one is SIMPLY a mortal one. 
+         * 
+         * @reference : http://www.mailinglistarchive.com/android-developers@googlegroups.com/msg17982.html
+         */
+        if(boardFields[position] == null){
+        	boardFields[position] = imageView;
+        }        
         return imageView;
     }
 
