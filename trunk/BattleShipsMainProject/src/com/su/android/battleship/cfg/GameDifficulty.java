52483dded20c68d33ac50b65679110e7c2ca06d2
i_stoykov@abv.bg <i_stoykov@abv.bg@45fd3aa4-62c5-c62a-3edd-0140abc20f17>
2010-05-02 19:42:10 +0000

    game preferences api and menu added. the menu is with experimental design

---
+++ trunk/BattleShipsMainProject/src/com/su/android/battleship/cfg/GameDifficulty.java
@@ -0,0 +1,25 @@
+package com.su.android.battleship.cfg;
+
+/**
+ * Enum, representing difficulty levels in the game
+ * @author Ivaylo Stoykov
+ */
+public enum GameDifficulty {
+	EASY(1),
+	NORMAL(2),
+	HARD(3);
+	
+	// future use
+	private int mInfo;
+	
+	GameDifficulty(int aInfo) {
+		mInfo = aInfo;
+	}
+	
+	/**
+	 * @return some info, which could be used in the future
+	 */
+	public int getInfo() {
+		return mInfo;
+	}
+}
